7a058f7a122c801434b2242ffbabee682f185b72
nikhil.shirsath <nikhil.shirsath@sumasoft.net>
2026-09-08 13:18:04 +0530

    Initial setUp with datbase connectivity

---
+++ hmis-service/hmis-service/src/main/java/com/suma/hmis_service/HmisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.suma.hmis_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmisServiceApplication.class, args);
+	}
+
+}
---
+++ hmis-service/hmis-service/src/test/java/com/suma/hmis_service/HmisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suma.hmis_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
